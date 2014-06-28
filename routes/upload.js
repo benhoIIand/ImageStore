dc66e1057fcd4034ba1a021f0a37bbbec0981dbb
Ben Holland <benholland99@gmail.com>
2014-06-28 02:10:53 +0100

    Added path normalization for uploading images

--- routes/upload.js
+++ routes/upload.js
@@ -1,18 +1,14 @@
 var fs = require('fs'),
+    path = require('path'),
     crypto = require('crypto'),
     request = require('request');
-
-var getExtension = function(filename) {
-    var files = filename.split('.');
-    return files.length == 1 ? '' : files[files.length-1];
-};
 
 var downloadImage = function(uri, cb) {
     request.head(uri, function(err, res) {
         if(err) console.log(err);
 
         var size = parseInt(res.headers['content-length']);
-        var filename = 'store/tmp/image.'+ (+new Date()) + '.'+ Math.floor(Math.random() * 100000000000000) + '.'+ getExtension(uri);
+        var filename = path.normalize(__dirname +'/../store/tmp/image.'+ (+new Date()) + '.'+ Math.floor(Math.random() * 100000000000000) + path.extname(uri));
 
         request(uri).pipe(fs.createWriteStream(filename)).on('close', function() {
             cb(filename, size);
@@ -22,11 +18,11 @@ var downloadImage = function(uri, cb) {
 
 var saveImage = function(tempFilename, size, cb) {
     fs.readFile(tempFilename, { encoding: 'utf8' }, function(err, data) {
-        var ext = getExtension(tempFilename);
-        var hashfilename = crypto.createHash('md5').update(data, 'utf8').digest('hex').substring(0, 16) +'.'+ ext;
+        var ext = path.extname(tempFilename);
+        var hashfilename = crypto.createHash('md5').update(data, 'utf8').digest('hex').substring(0, 16) + ext;
         var filename = hashfilename.replace('/tmp/', '/');
 
-        fs.rename(tempFilename, 'store/'+ filename, function(err) {
+        fs.rename(tempFilename, path.normalize(__dirname +'/../store/'+ filename), function(err) {
             if(err) console.log(err);
 
             request.post('http://localhost:4000/api/assets', {
