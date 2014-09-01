38ab18173c8b5d10aefcd71b855277b9056e1923
Ben Holland <benholland99@gmail.com>
2014-09-01 22:08:53 +0100

    Using flex layout

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,7 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('sass', function () {
-    gulp.src('./src/Flubit/Bundle/Partner/WidgetBundle/Resources/public/scss/styles.scss')
+    gulp.src('./app/scss/build.scss')
         .pipe(sass())
-        .pipe(gulp.dest('./src/Flubit/Bundle/Partner/WidgetBundle/Resources/public/css'));
+        .pipe(gulp.dest('./app/css/'));
 });
