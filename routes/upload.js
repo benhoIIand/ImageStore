c2b000c35042ed0ad21af578d8d8f82e848eb951
Ben Holland <benholland99@gmail.com>
2014-10-19 22:06:16 +0100

    Removing query string from filename

--- routes/upload.js
+++ routes/upload.js
@@ -5,10 +5,10 @@ var fs = require('fs'),
 
 var downloadImage = function(uri, cb) {
     request.head(uri, function(err, res) {
-        if(err) console.log(err);
+        if (err) console.log(err);
 
         var size = parseInt(res.headers['content-length']);
-        var filename = path.normalize(__dirname +'/../store/tmp/image.'+ (+new Date()) + '.'+ Math.floor(Math.random() * 100000000000000) + path.extname(uri));
+        var filename = path.normalize(__dirname + '/../store/tmp/image.' + (+new Date()) + '.' + Math.floor(Math.random() * 100000000000000) + path.extname(uri)).split('?')[0];
 
         request(uri).pipe(fs.createWriteStream(filename)).on('close', function() {
             cb(filename, size);
@@ -17,13 +17,15 @@ var downloadImage = function(uri, cb) {
 };
 
 var saveImage = function(tempFilename, size, cb) {
-    fs.readFile(tempFilename, { encoding: 'utf8' }, function(err, data) {
+    fs.readFile(tempFilename, {
+        encoding: 'utf8'
+    }, function(err, data) {
         var ext = path.extname(tempFilename);
         var hashfilename = crypto.createHash('md5').update(data, 'utf8').digest('hex').substring(0, 16) + ext;
         var filename = hashfilename.replace('/tmp/', '/');
 
-        fs.rename(tempFilename, path.normalize(__dirname +'/../store/'+ filename), function(err) {
-            if(err) console.log(err);
+        fs.rename(tempFilename, path.normalize(__dirname + '/../store/' + filename), function(err) {
+            if (err) console.log(err);
 
             request.post('http://localhost:4000/api/assets', {
                 form: {
@@ -42,7 +44,7 @@ module.exports = {
     byUrl: function(req, res) {
         var uri = req.query.url;
 
-        if(!uri) {
+        if (!uri) {
             res.json({
                 error: 'No url provided'
             });
@@ -51,7 +53,7 @@ module.exports = {
 
         downloadImage(uri, function(tempFilename, size) {
             saveImage(tempFilename, size, function(filename) {
-                res.redirect('/'+ filename);
+                res.redirect('/' + filename);
             });
         });
     }
